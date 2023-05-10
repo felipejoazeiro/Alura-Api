8d710e8190361342519a7ebca05d79aacd2c7cf3
Felipe Joazeiro <felipe.joazeiro@ictcondor.com.br>
2023-05-10 08:20:57 -0300

    3-Documentação da API

--- api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -8,16 +8,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.AgendaDeConsulta;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 
 // Trecho de código suprimido
 
 @RestController
 @RequestMapping("consultas")
+@SecurityRequirement(name="bearer-key")
 public class ConsultaController {
 
     @Autowired
